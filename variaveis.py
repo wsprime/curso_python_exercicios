cdc2a64d913818a9d4861df41e150c875060cce3
WS Prime <contato@wsprime.com.br>
2023-02-13 20:16:47 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,15 @@
+# Estes são exemploes de tipos de variáveis
+var_inteira = 10
+var_float = 10.75
+var_bool = True
+var_bool2 = False
+var_dicionario = {'colunaA': 10, 'colunaB': 200, 'colunaC':100.75, 'colunaD': False}
+var_string = "Régis Albuquerque"
+var_string2 = 'Marcus Marçal'
+var_string3 = "Daniel d' Almeida"
+
+''' Este é um comentário de
+duas linhas '''
+
+print(var_dicionario['colunaD'])
+
