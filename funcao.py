ec3fcf7ee0e6fc1fcfa00e5f606759004cd94759
WS Prime <contato@wsprime.com.br>
2023-02-16 19:40:48 -0300

    Create funcao.py

---
+++ funcao.py
@@ -0,0 +1,20 @@
+print("Meu programa que usa função")
+# Definição da Função multiplica
+def multiplica(num1, num2):
+    multiplicacao = num1 * num2
+    return multiplicacao
+# Definição da Função soma
+def soma(num1, num2):
+    soma = num1 + num2
+    return soma
+
+# Chamada da função multiplica
+resultado1 = multiplica(2, 5)
+print("O resultado de 2 * 5 é:")
+print(resultado1)
+
+# Chamada da função soma
+resultado2 = soma(2, 5)
+print("O resultado de 2 + 5 é:", resultado2)
+resultado3 = soma(10, 20)
+print("O resultado de 10 + 20 é:", resultado3)
