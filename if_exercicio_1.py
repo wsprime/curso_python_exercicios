a71be7dec29dbb52f62682dcb5c2adc1ca2bba3d
Regis Albuquerque <regis.albuquerque1@gmail.com>
2023-02-20 20:00:51 -0300

    Create if_exercicio_1.py

---
+++ if_exercicio_1.py
@@ -0,0 +1,5 @@
+# Faça um programa em python que receba o nome e a nota de um aluno
+# Use a função input() e use a função int().
+
+# Caso a nota seja maior ou igual a 7, imprimir Aprovado
+# Caso seja menor, imprimir Recuperação
