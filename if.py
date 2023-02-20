4f96a2c99e9c3d2350e09141422561391ac048cf
Regis Albuquerque <regis.albuquerque1@gmail.com>
2023-02-20 18:45:55 -0300

    Create if.py

---
+++ if.py
@@ -0,0 +1,13 @@
+nota = 8
+nome = 'Regis' 
+
+if nota >= 7:
+    print(" Voce passou! ")
+    print(f' Sua nota eh: {nota}, Eh maior ou igual a 7')
+    
+else:
+    print(" Voce reprovou! ")
+    print(f' Sua nota eh: {nota}, Eh menor que 7')
+
+print(" O programa terminou")
+    
