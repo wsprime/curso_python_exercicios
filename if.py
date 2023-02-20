9effc816f08300d0f3f7475d0a12b0f94c3eb8b3
Regis Albuquerque <regis.albuquerque1@gmail.com>
2023-02-20 19:57:39 -0300

    Update if.py

--- if.py
+++ if.py
@@ -1,9 +1,42 @@
-nota = 8
+nota = 6
 nome = 'Regis' 
 
-if nota >= 7:
+# IF ANINHADO
+if nota > 7:
     print(" Voce passou! ")
-    print(f' Sua nota eh: {nota}, Eh maior ou igual a 7')
+    print(f' Sua nota eh: {nota}, Eh maior 7')
+
+else:
+    if nota == 7:
+        print(" Voce passou! ")
+        print(f' Sua nota eh: {nota}, Eh igual a 7')
+    else:
+        print(" Voce reprovou! ")
+        print(f' Sua nota eh: {nota}, Eh menor que 7')
+
+# IF NORMAL   
+if nota > 7:
+    print(" Voce passou! ")
+    print(f' Sua nota eh: {nota}, Eh maior 7')
+
+elif nota == 7:
+    print(" Voce passou! ")
+    print(f' Sua nota eh: {nota}, Eh igual a 7')
+    
+else:
+    print(" Voce reprovou! ")
+    print(f' Sua nota eh: {nota}, Eh menor que 7')
+
+# IF COM DUAS VARIAVEIS
+nota = 5
+final = 4
+if nota >=7:
+    print(" Voce passou! ")
+    print(f' Sua nota eh: {nota}, Eh maior 7')
+
+elif nota >= 5 and final >= 5:
+    print(" Voce passou na final! ")
+    print(f' Sua nota eh: {nota}, E sua nota da final é: {final}')
     
 else:
     print(" Voce reprovou! ")
