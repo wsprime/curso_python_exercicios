59a5ff5c504d08c3cbae3c84aabdc6e60cd1e39f
Regis Albuquerque <regis.albuquerque1@gmail.com>
2023-02-24 20:10:29 -0300

    Introducao ao While

---
+++ while.py
@@ -0,0 +1,12 @@
+# Iniciando minha variável de controle
+contador = 0
+
+# Defino o while com a condição
+while contador <=4:
+    # Bloco de repetição dentro do while
+    print("Iteração: ",contador)
+    print("Hello World!")
+    contador = contador + 1
+
+# while terminou
+print("Terminou")
